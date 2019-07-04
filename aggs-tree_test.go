9b359f64a1156a19de90f18010a32cf3cc820720
Evghenii Maslennikov <evghenii.maslennikov@gmail.com>
2019-07-04 14:19:09 +0300

    simple tests

--- aggs-tree_test.go
+++ aggs-tree_test.go
@@ -5,13 +5,14 @@ import (
 	"github.com/aahainc/aggretastic"
 	"github.com/olivere/elastic/v7"
 	. "github.com/onsi/ginkgo"
-	"log"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("AggsTree", func() {
 
 	Context("ExtractLeafs", func() {
 
+		// Tests are really poor for now @todo: tests
 		It("should work", func() {
 			agg := aggretastic.NewFilterAggregation().Filter(elastic.NewTermQuery("a", "b"))
 			agg.Inject(aggretastic.NewChildrenAggregation().Type("child"), "down")
@@ -19,13 +20,17 @@ var _ = Describe("AggsTree", func() {
 			agg.Inject(aggretastic.NewChildrenAggregation().Type("foobar"), "filtered", "deeper")
 			agg.Inject(aggretastic.NewChildrenAggregation().Type("foobarx"), "filtered", "deeper-x")
 
-			log.Println("leafs ", agg.ExtractLeafPaths())
+			leafPaths := agg.ExtractLeafPaths()
+			Expect(leafPaths).To(HaveLen(3))
+			Expect(leafPaths[0]).To(Equal([]string{"down"}))
+			Expect(leafPaths[1]).To(Equal([]string{"filtered", "deeper"}))
+			Expect(leafPaths[2]).To(Equal([]string{"filtered", "deeper-x"}))
 
-			p, e := agg.InjectSafe(
+			resultPaths, err := agg.InjectSafe(
 				aggretastic.NewChildrenAggregation().Type("foobar").SubAggregation(
 					"deeper",
 					aggretastic.NewChildrenAggregation().Type("foobar").SubAggregation(
-						"semi-final",
+						"pre-final",
 						aggretastic.NewFilterAggregation().Filter(elastic.NewTermQuery("cc", "ee")).SubAggregation(
 							"final", aggretastic.NewSumAggregation().Field("abc"),
 						),
@@ -33,12 +38,16 @@ var _ = Describe("AggsTree", func() {
 				),
 				"filtered",
 			)
-			log.Println("err ", e)
-			log.Println("result paths ", p)
 
+			Expect(err).ShouldNot(HaveOccurred())
+
+			Expect(resultPaths).To(HaveLen(1))
+			Expect(resultPaths[0]).To(Equal([]string{"filtered", "deeper", "pre-final", "final"}))
+
+			// how agg was changed
 			s, _ := agg.Source()
 			j, _ := json.Marshal(s)
-			log.Println(string(j))
+			Expect(string(j)).To(Equal(`{"aggregations":{"down":{"children":{"type":"child"}},"filtered":{"aggregations":{"deeper":{"aggregations":{"pre-final":{"aggregations":{"final":{"sum":{"field":"abc"}}},"filter":{"term":{"cc":"ee"}}}},"children":{"type":"foobar"}},"deeper-x":{"children":{"type":"foobarx"}}},"filter":{"term":{"b":"c"}}}},"filter":{"term":{"a":"b"}}}`))
 		})
 	})
 })
